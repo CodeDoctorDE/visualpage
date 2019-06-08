e928a273053772fb63a141d37ab523a65be5a23b
CodeDoctorDE <proficodergames@gmail.com>
2019-06-08 09:07:10 +0200

    add custom prefix method

--- api/localization.js
+++ api/localization.js
@@ -36,3 +36,41 @@ function localize(file,errorCallback,successCallback) {
         }
     });
 }
+function localize(prefix,file,errorCallback,successCallback) {
+    $.ajax({
+        url:file,
+        type:'HEAD',
+        error: function()
+        {
+            errorCallback();
+        },
+        success: function()
+        {
+            $.getJSON(file, function (data) {
+                $(".lang").each(function () {
+                    var currentData = data;
+                    $.each(this.attributes, function () {
+                        if (this.specified) {
+                            if (this.value.startsWith(prefix)) {
+                                this.value = this.value.slice(prefix.length);
+                                this.value.split("/").forEach(function (index) {
+                                    currentData = currentData[index];
+                                });
+                                this.value = currentData;
+                            }
+                        }
+                    });
+                    if ($(this).text().startsWith(prefix)) {
+        
+                        $(this).text($(this).text().slice(prefix.length));
+                        $(this).text().split(" ").forEach(function (index) {
+                            currentData = currentData[index];
+                        });
+                        $(this).text(currentData);
+                    }
+                });
+            });
+            successCallback();
+        }
+    });
+}
