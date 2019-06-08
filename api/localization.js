2c5c03c24d549d9b6118a7dd97a4a3188da92a01
CodeDoctorDE <proficodergames@gmail.com>
2019-06-08 09:51:12 +0200

    fix

--- api/localization.js
+++ api/localization.js
@@ -1,42 +1,8 @@
-function localize(file,errorCallback,successCallback) {
-    $.ajax({
-        url:file,
-        type:'HEAD',
-        error: function()
-        {
-            errorCallback();
-        },
-        success: function()
-        {
-            $.getJSON(file, function (data) {
-                $(".lang").each(function () {
-                    var currentData = data;
-                    $.each(this.attributes, function () {
-                        if (this.specified) {
-                            if (this.value.startsWith("#lang ")) {
-                                this.value = this.value.slice(6);
-                                this.value.split("/").forEach(function (index) {
-                                    currentData = currentData[index];
-                                });
-                                this.value = currentData;
-                            }
-                        }
-                    });
-                    if ($(this).text().startsWith("#lang ")) {
-        
-                        $(this).text($(this).text().slice(6));
-                        $(this).text().split(" ").forEach(function (index) {
-                            currentData = currentData[index];
-                        });
-                        $(this).text(currentData);
-                    }
-                });
-            });
-            successCallback();
-        }
-    });
-}
-function localize(prefix,file,errorCallback,successCallback) {
+
+function localize(file,errorCallback,successCallback,prefix) {
+    if (prefix === undefined) {
+        prefix = '#lang ';
+    }
     $.ajax({
         url:file,
         type:'HEAD',
